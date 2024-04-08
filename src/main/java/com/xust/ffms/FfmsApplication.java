07d001a44256bbc8de1236f81155b73eea044b66
dreamfireyu <poissonyzr@outlook.com>
2024-04-08 14:54:55 -0400

    java code

---
+++ src/main/java/com/xust/ffms/FfmsApplication.java
@@ -0,0 +1,23 @@
+package com.xust.ffms;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@MapperScan("com.xust.ffms.dao")
+//@EnableTransactionManagement
+@SpringBootApplication
+public class FfmsApplication extends SpringBootServletInitializer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FfmsApplication.class, args);
+        System.out.println("<============启动成功===============>");
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder applicationBuilder) {
+        return applicationBuilder.sources(FfmsApplication.class);
+    }
+}
